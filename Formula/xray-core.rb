47064b3c45f336aac327f32b2d9dd738144156e9
N4FA <75566284+N4FA@users.noreply.github.com>
2020-12-16 05:10:19 +0000

    Create xray-core.rb

---
+++ Formula/xray-core.rb
@@ -0,0 +1,46 @@
+class XrayCore < Formula
+  desc "Xray, Penetrates Everything. Also the best xray-core, with XTLS support. Fully compatible configuration."
+  homepage "https://t.me/projectXray"
+  url "https://github.com/XTLS/Xray-core/releases/download/v1.1.3/Xray-macos-64.zip"
+  sha256 "e1191b32d33798965a3dfb4c34ee09c778e3bafafa0e55f6f2d46e8bd6f748b9"
+  license "MPL-2.0"
+
+
+  def install
+    bin.install "xray"
+    pkgshare.install "geoip.dat"
+    pkgshare.install "geosite.dat"
+
+  end
+
+  plist_options manual: "xray -config=#{HOMEBREW_PREFIX}/etc/xray/config.json"
+
+  def plist; <<~EOS
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{bin}/xray</string>
+      </array>
+    </dict>
+  </plist>
+  EOS
+  end
+
+  test do
+
+  end
+  
+  def caveats
+    <<~EOS
+    EOS
+  end
+end
