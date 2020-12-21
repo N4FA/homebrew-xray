05a1339165748105558182c7b5128aa7289855bd
N4FA <75566284+N4FA@users.noreply.github.com>
2020-12-21 06:48:09 +0000

    Update xray-core.rb

--- Formula/xray-core.rb
+++ Formula/xray-core.rb
@@ -15,32 +15,40 @@ class XrayCore < Formula
 
   plist_options manual: "xray -config=#{HOMEBREW_PREFIX}/etc/xray/config.json"
 
-  def plist; <<~EOS
-  <?xml version="1.0" encoding="UTF-8"?>
-  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-  <plist version="1.0">
-    <dict>
-      <key>KeepAlive</key>
-      <true/>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{bin}/xray</string>
-      </array>
-    </dict>
-  </plist>
-  EOS
+  def plist; 
+
+   <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <true/>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{bin}/xray</string>
+            <string>run</string>
+            <string>-config</string>
+            <string>#{etc}/xray/config.json</string>
+          </array>
+        </dict>
+      </plist>
+    EOS
+    
+    
   end
 
   test do
-
+  system "#{bin}/xray", "version"
   end
   
   def caveats
-    <<~EOS
+   <<~EOS
+      Get example configs at https://github.com/XTLS/Xray-examples  and save config.json to #{etc}/xray
     EOS
   end
 end
