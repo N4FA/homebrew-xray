0300a60c7e5351040f101e939905aa13a3db8458
N4FA <75566284+N4FA@users.noreply.github.com>
2021-02-09 01:37:23 +0800

    Update xray-core.rb

--- Formula/xray-core.rb
+++ Formula/xray-core.rb
@@ -3,10 +3,10 @@ class XrayCore < Formula
   homepage "https://t.me/projectXray"
   if Hardware::CPU.intel?
     url "https://github.com/XTLS/Xray-core/releases/download/v1.2.4/Xray-macos-64.zip" # intel_url
-    sha256 "938c2d12734d2b0289a08e476df9fe913c806e28204c8645da727eb29e7a534e" # intel
+    sha256 "938c2d12734d2b0289a08e476df9fe913c806e28204c8645da727eb29e7a534e" # intel_sha
   else
     url "https://github.com/XTLS/Xray-core/releases/download/v1.2.4/Xray-macos-arm64-v8a.zip" # arm_url
-    sha256 "" # arm
+    sha256 "d55ebec9e17247ea2e0e32d68a7d7f98e71886ef768fc36873f32e1960c93625" # arm_sha
   end
   version "1.2.4"
   license "MPL-2.0"
